a5ae7e3095714fde37283c8abf47115fba02299e
Priyanka Saha <priyanka.nov15@gmail.com>
2018-04-02 21:42:33 -0500

    Update rule_checker.py

--- rule_checker.py
+++ rule_checker.py
@@ -16,8 +16,13 @@ data = pandas.read_table(fname, header=None, delim_whitespace=True)
 
 
 rulecount = 0 #Count the number of rules
-rule_left = [] #list to keep the left parts of the Rules
-rule_right = [] #list to keep the right parts of the Rules
+rule_left = [] #list to store the left parts of the Rules
+rule_right = [] #list to store the right parts of the Rules
+rule_left_conditions = [] #list to store the conditions of the rules
+rule_left_con = [] #final list to store the attribute-value pairs
+rule_right_con = [] #final list to store the concept-value
+attribute = [] #list to store the attributes
+value = [] #list to store the values
 
 #Looping through the lines of the Rule file
 for x in lines:
@@ -29,10 +34,42 @@ for x in lines:
 		rule_left.append(rulesep[0]) #updating the Left side of the rules
 		rule_right.append(rulesep[1]) #updating the Right side of the rules
 
-print("\n\n", rule_left)
-print("\n\n", rule_right)
 
-print("\n\nNumber of Rules: ", rulecount)
+#Splitting the left side of the rule by '&'
+for x in rule_left:
+	rulesep2 = x.split("&")
+	length = len(rulesep2)
+	for y in range(0,length):
+		rule_left_conditions.append(rulesep2[y])
+
+#Stripping the conditions of the rules to remove whitespaces and brackets
+for x in rule_left_conditions:
+	x = x.strip()
+	x = x.lstrip("(")
+	x = x.rstrip(")")
+	rule_left_con.append(x)
+
+#Stripping the concept of the rules to remove whitespaces, brackets and new line
+for x in rule_right:
+	x = x.strip()
+	x = x.lstrip("(")
+	x = x.rstrip(")")
+	x = x.rstrip("\n")
+	rule_right_con.append(x)
+
+
+for x in rule_left_con:
+	sep1 = x.split(",")
+	attribute.append(sep1[0])
+	value.append(sep1[1])
+
+for x in rule_right_con:
+	sep1 = x.split(",")
+	attribute.append(sep1[0])
+	value.append(sep1[1])
+
+print("\n", attribute)
+print("\n", value)
 
 data.drop(data.index[0],inplace=True)
 data.iloc[0] = data.iloc[0].replace('[','') #remove leading bracket from first row
@@ -50,8 +87,11 @@ for i in range(0,colnum_old-1):
 
 data.dropna(axis=1, how='any',inplace=True) #drop all the columns having any NaN values
 
-print(data)
+#print(data)
 rownum = data.shape[0] #No. of Rows in the dataframe
 colnum = data.shape[1] #No. of Columns in the dataframe
+print("\n\n----This report was created from the Rule file", rname, "and from the Data file", fname, "----")
 print("\n\nThe total number of cases: ", rownum-1)
-print("\n\nThe total number of Attributes: ", colnum-1)
+print("\nThe total number of Attributes: ", colnum-1)
+print("\nThe total number of Rules: ", rulecount)
+print("\nThe total number of Conditions: ", len(rule_left_conditions))
